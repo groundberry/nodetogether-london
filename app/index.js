359258fa579fa3b0dc7c8bc6a0eb6c002bcfbf71
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-07-20 21:54:33 +0100

    add motivations and pick-one packages to the app

--- app/index.js
+++ app/index.js
@@ -1,12 +1,17 @@
 // write a rule to say hello
 // when I hit /hello
 
+var pickOne = require('pick-one');
+var motivations = require('motivations');
+
 var Express = require('express');
 var app = Express();
 
+
+
 app.get('/hello', function(request, response) {
   console.log("request to /hello");
-  response.send("Hello!");
+  response.send(motivations);
 });
 
 module.exports = app;
